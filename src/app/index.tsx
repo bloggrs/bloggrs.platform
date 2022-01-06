51d118aa99a78257a44f3bfa40bfabd59e45535c
Gjergj Kadriu <gjergjk71@gmail.com>
2022-01-06 18:42:42 +0100

    push

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,6 +13,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from '../styles/global-styles';
 
 import { HomePage } from './pages/HomePage/Loadable';
+import { LoginPage } from './pages/LoginPage/Loadable';
 import { NotFoundPage } from './pages/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
 
@@ -30,9 +31,10 @@ export function App() {
 
       <Switch>
         <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
+        <Route exact path={process.env.PUBLIC_URL + '/login'} component={LoginPage} />
         <Route component={NotFoundPage} />
       </Switch>
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
     </BrowserRouter>
   );
 }
