0f4daac5e711d286249a42a9e715bc46c0f0bdeb
Gjergj Kadriu <gjergjk71@gmail.com>
2022-04-09 19:25:56 +0200

    sidebar-fixed

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -18,57 +18,70 @@ export const _Sidebar = ({ collapse, style, ...rest }: any) => {
   }
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top: 104,
-        ...style,
-      }}
-      className={
-        'h-screen max-h-screen max-w-52 w-24 py-5 bg-slate-700 center-items'
-      }
-    >
-      <Link to={getPathname(blog_id, '/')}>
-        <CenteredImage
-          className="py-3 cursor-pointer"
-          src="/dist/static/sidebar/icons8-dashboard-80.png"
-        />
-      </Link>
-      <Link to={getPathname(blog_id, '/posts')}>
-        <CenteredImage
-          className="py-3 cursor-pointer"
-          src="/dist/static/sidebar/icons8-page-80.png"
-        />
-      </Link>
-      <Link to={getPathname(blog_id, '/comments')}>
-        <CenteredImage
-          className="py-3 cursor-pointer"
-          src="/dist/static/sidebar/icons8-chat-bubble-80.png"
-        />
-      </Link>
-      <Link to={getPathname(blog_id, '/team-members')}>
-        <CenteredImage
-          className="py-3 cursor-pointer"
-          src="/dist/static/sidebar/icons8-team-80.png"
-        />
-      </Link>
-      <Link to={getPathname(blog_id, '/settings')}>
-        <CenteredImage
-          className="py-3 cursor-pointer"
-          src="/dist/static/sidebar/icons8-settings-80.png"
-        />
-      </Link>
+    <React.Fragment>
       <div
         style={{
-          background: '#164666 0% 0% no-repeat padding-box;',
+          ...style,
+          position: 'fixed',
+          top: 0,
+          zIndex: -1,
+          background:
+            '0% 0% no-repeat padding-box padding-box rgb(28, 88, 129);',
         }}
-        className="fixed bottom-0 bg-white h-24 py-5 w-24 bg-slate-900"
+        className={''}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          top: 104,
+          ...style,
+        }}
+        className={
+          'h-screen max-h-screen max-w-52 w-24 py-5 bg-slate-700 center-items'
+        }
       >
-        <div className="cursor-pointer align-middle w-14 h-14 m-auto py-4 mx-5 rounded-full bg-slate-700 text-center ">
-          <span className="py-4 font-medium text-white">GK</span>
+        <Link to={getPathname(blog_id, '/')}>
+          <CenteredImage
+            className="py-3 cursor-pointer"
+            src="/dist/static/sidebar/icons8-dashboard-80.png"
+          />
+        </Link>
+        <Link to={getPathname(blog_id, '/posts')}>
+          <CenteredImage
+            className="py-3 cursor-pointer"
+            src="/dist/static/sidebar/icons8-page-80.png"
+          />
+        </Link>
+        <Link to={getPathname(blog_id, '/comments')}>
+          <CenteredImage
+            className="py-3 cursor-pointer"
+            src="/dist/static/sidebar/icons8-chat-bubble-80.png"
+          />
+        </Link>
+        <Link to={getPathname(blog_id, '/team-members')}>
+          <CenteredImage
+            className="py-3 cursor-pointer"
+            src="/dist/static/sidebar/icons8-team-80.png"
+          />
+        </Link>
+        <Link to={getPathname(blog_id, '/settings')}>
+          <CenteredImage
+            className="py-3 cursor-pointer"
+            src="/dist/static/sidebar/icons8-settings-80.png"
+          />
+        </Link>
+        <div
+          style={{
+            background: '#164666 0% 0% no-repeat padding-box;',
+          }}
+          className="fixed bottom-0 bg-white h-24 py-5 w-24 bg-slate-900"
+        >
+          <div className="cursor-pointer align-middle w-14 h-14 m-auto py-4 mx-5 rounded-full bg-slate-700 text-center ">
+            <span className="py-4 font-medium text-white">GK</span>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
