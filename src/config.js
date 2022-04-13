436661b5fe072c222d25171edef11c36300c2605
Gjergj Kadriu <gjergjk71@gmail.com>
2022-04-13 15:48:21 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const IS_LOCALHOST = window.location.href.indexOf('localhost') !== -1;
 
-export const API_URL = 'http://localhost:5500';
+// export const API_URL = 'http://localhost:5500';
 
-// export const API_URL = IS_LOCALHOST
-//   ? 'https://bloggrs-api-i1.herokuapp.com'
-//   : 'https://bloggrs-api-i1.herokuapp.com';
-// // ? 'http://localhost:5500'
+export const API_URL = IS_LOCALHOST
+  ? 'http://localhost:5500'
+  : 'https://bloggrs-api-i1.herokuapp.com';
+// ? 'http://localhost:5500'
