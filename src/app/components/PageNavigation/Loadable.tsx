8386d2a110c200d7dfa8d179f8ca85e6e44658e5
gjergjkadriu <gjergjk71@gmail.com>
2022-07-19 18:09:21 +0200

    page-navigation

---
+++ src/app/components/PageNavigation/Loadable.tsx
@@ -0,0 +1,11 @@
+/**
+ * Asynchronously loads the component for PageNavigation
+ */
+
+ import { lazyLoad } from 'utils/loadable';
+
+ export const PageNavigation = lazyLoad(
+   () => import('./index'),
+   module => module.PageNavigation,
+ );
+ 
---
+++ src/app/components/PageNavigation/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PageNavigation = ({ label, items }) => {
+    const items_ = [
+        {
+            label: "Bloggrs",
+            to: "/"
+        },
+        ...items
+    ]
+    const ITEMS = items_.map(({ label, active, to }) => {
+        const className = active ? "breadcrumb-item active" : "breadcrumb-item"
+        const LinkOrNone: any = active ? ({ children }) => <>{children}</> : Link
+        return (
+            <li className={className}>
+                <LinkOrNone to={to}>{label}</LinkOrNone>
+            </li>
+        )
+    })
+    return (
+        <div className="col">
+            <h4 className="page-title">{label}</h4>
+            <ol className="breadcrumb">
+                {ITEMS}
+            </ol>
+        </div>
+    )
+}
+
+export { PageNavigation };
