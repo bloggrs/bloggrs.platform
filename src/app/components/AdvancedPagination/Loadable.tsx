14b3aed80aac853b158accaa2ca8a2514dd32cc5
gjergjkadriu <gjergjk71@gmail.com>
2022-07-19 18:09:31 +0200

    advanced-pagination

---
+++ src/app/components/AdvancedPagination/Loadable.tsx
@@ -0,0 +1,11 @@
+/**
+ * Asynchronously loads the component for AdvancedPagination
+ */
+
+ import { lazyLoad } from 'utils/loadable';
+
+ export const AdvancedPagination = lazyLoad(
+   () => import('./index'),
+   module => module.AdvancedPagination,
+ );
+ 
